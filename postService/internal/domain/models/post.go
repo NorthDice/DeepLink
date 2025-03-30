9e3b78217671f0504c82efe064dde0f6c8f2585e
NorthDice <mister.balychev@gmail.com>
2025-03-30 16:37:48 +0300

    Wrote mathods to implement in service lawyer

--- postService/internal/domain/models/post.go
+++ postService/internal/domain/models/post.go
@@ -1,1 +1,32 @@
 package models
+
+import "time"
+
+type Post struct {
+	ID         int64
+	UserID     int64
+	Content    string
+	MediaURLs  []string
+	CreatedAt  time.Time
+	UpdatedAt  *time.Time
+	LikesCount int32
+	Comments   []Comment
+	IsDeleted  bool
+	DeletedAt  *time.Time
+}
+
+type Comment struct {
+	ID        int64
+	UserID    int64
+	Content   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Attachment struct {
+	Type      string
+	URL       string
+	Thumbnail string
+	Size      int64
+	CreatedAt time.Time
+}
--- postService/internal/services/post/post.go
+++ postService/internal/services/post/post.go
@@ -1,1 +1,62 @@
 package post
+
+import (
+	"context"
+	"github.com/yourusername/deepLink/post-service/internal/domain/models"
+	"log/slog"
+)
+
+type Post struct {
+	log            *slog.Logger
+	pstCreator     PostCreator
+	pstProvider    PostProvider
+	pstModerator   PostModerator
+	pstInteraction PostInteraction
+	appProvider    AppProvider
+}
+
+type PostCreator interface {
+	CreatePost(
+		ctx context.Context,
+		userID int64,
+		content string,
+		mediaURLs []string,
+	) (postID int64, err error)
+}
+
+type PostProvider interface {
+	GetPost(ctx context.Context, postID int64) (*models.Post, error)
+	ListPosts(ctx context.Context, page, pageSize int32) ([]*models.Post, error)
+}
+
+type PostModerator interface {
+	DeletePost(ctx context.Context, postID int64) (bool, error)
+}
+
+type PostInteraction interface {
+	LikePost(ctx context.Context, userID, postID int64) (bool, error)
+	AddComment(ctx context.Context, postID, userID int64, content string) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int32) (models.App, error)
+}
+
+// New returns a new instance of the Post service.
+func New(
+	log *slog.Logger,
+	postCreator PostCreator,
+	postProvider PostProvider,
+	postModerator PostModerator,
+	postInteraction PostInteraction,
+	appProvider AppProvider,
+) *Post {
+	return &Post{
+		log:            log,
+		pstCreator:     postCreator,
+		pstProvider:    postProvider,
+		pstModerator:   postModerator,
+		pstInteraction: postInteraction,
+		appProvider:    appProvider,
+	}
+}
