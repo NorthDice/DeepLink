2e345e1866b50057a5e193f19906d5bfbc725ac6
NorthDice <mister.balychev@gmail.com>
2025-03-30 18:18:15 +0300

    Add validation to transport layer

--- kafkaService/internal/grpc/post/server.go
+++ kafkaService/internal/grpc/post/server.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	kafkav1 "github.com/NorthDice/DeepLink/protos/gen/go/kafka"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"unicode/utf8"
 )
 
 type Producer interface {
@@ -24,6 +27,11 @@ func Register(gRPCServer *grpc.Server) {
 }
 
 func (s *serverAPI) CreatePost(ctx context.Context, req *kafkav1.PostCreateEvent) (*emptypb.Empty, error) {
+
+	if err := validateCreatePost(req); err != nil {
+		return nil, err
+	}
+
 	if err := s.producer.ProducePostCreated(ctx, req); err != nil {
 		return nil, err
 	}
@@ -31,6 +39,11 @@ func (s *serverAPI) CreatePost(ctx context.Context, req *kafkav1.PostCreateEvent
 }
 
 func (s *serverAPI) DeletePost(ctx context.Context, req *kafkav1.PostDeletedEvent) (*emptypb.Empty, error) {
+
+	if err := validateDeletePost(req); err != nil {
+		return nil, err
+	}
+
 	if err := s.producer.ProducePostDeleted(ctx, req); err != nil {
 		return nil, err
 	}
@@ -38,6 +51,11 @@ func (s *serverAPI) DeletePost(ctx context.Context, req *kafkav1.PostDeletedEven
 }
 
 func (s *serverAPI) LikePost(ctx context.Context, req *kafkav1.PostLikedEvent) (*emptypb.Empty, error) {
+
+	if err := validateLikePost(req); err != nil {
+		return nil, err
+	}
+
 	if err := s.producer.ProducePostLiked(ctx, req); err != nil {
 		return nil, err
 	}
@@ -46,8 +64,59 @@ func (s *serverAPI) LikePost(ctx context.Context, req *kafkav1.PostLikedEvent) (
 }
 
 func (s *serverAPI) AddComment(ctx context.Context, req *kafkav1.CommentAddedEvent) (*emptypb.Empty, error) {
+
+	if err := validateAddComment(req); err != nil {
+		return nil, err
+	}
+
 	if err := s.producer.ProduceCommentAdded(ctx, req); err != nil {
 		return nil, err
 	}
 	return &emptypb.Empty{}, nil
+}
+
+func validateCreatePost(req *kafkav1.PostCreateEvent) error {
+	if req.PostId <= 0 {
+		return status.Error(codes.InvalidArgument, "post_id must be positive")
+	}
+	if req.UserId <= 0 {
+		return status.Error(codes.InvalidArgument, "user_id must be positive")
+	}
+	if utf8.RuneCountInString(req.Content) == 0 {
+		return status.Error(codes.InvalidArgument, "content cannot be empty")
+	}
+
+	return nil
+}
+func validateDeletePost(req *kafkav1.PostDeletedEvent) error {
+
+	if req.PostId <= 0 {
+		return status.Error(codes.InvalidArgument, "post_id must be positive")
+	}
+
+	return nil
+}
+
+func validateLikePost(req *kafkav1.PostLikedEvent) error {
+	if req.PostId <= 0 {
+		return status.Error(codes.InvalidArgument, "post_id must be positive")
+	}
+	if req.UserId <= 0 {
+		return status.Error(codes.InvalidArgument, "user_id must be positive")
+	}
+
+	return nil
+}
+
+func validateAddComment(req *kafkav1.CommentAddedEvent) error {
+	if req.PostId <= 0 {
+		return status.Error(codes.InvalidArgument, "post_id must be positive")
+	}
+	if req.UserId <= 0 {
+		return status.Error(codes.InvalidArgument, "user_id must be positive")
+	}
+	if utf8.RuneCountInString(req.Content) == 0 {
+		return status.Error(codes.InvalidArgument, "content cannot be empty")
+	}
+	return nil
 }
